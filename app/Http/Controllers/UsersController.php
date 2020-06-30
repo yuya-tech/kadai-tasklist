73d8db16b619bd4afd82f5edf97cec405fd797ad
yuya_10969 <genmaimai140@gmail.com>
2020-06-30 22:33:12 +0900

    ユーザーページの作成

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,13 @@ class UsersController extends Controller
             'users' => $users,
         ]);
     }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
 }
