48fd1da0f979b4d7e131649a1265dd43c6733448
yuya_10969 <genmaimai140@gmail.com>
2020-07-02 22:42:27 +0900

    welcome.bladeの修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,31 +2,7 @@
 
 @section('content')
     @if (Auth::check())
-        <div class="row">
-            <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
-                    </div>
-                </div>
-            </aside>
-            <div class="col-sm-8">
-                @if (Auth::id() == $user->id)
-                    {!! Form::open(['route' => 'tasks.store']) !!}
-                        <div class="form-group">
-                            {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
-                            {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
-                        </div>
-                    {!! Form::close() !!}
-                @endif
-                @if (count($tasks) > 0)
-                    @include('tasks.tasks', ['tasks' => $tasks])
-                @endif
-            </div>
-        </div>
+        {{ Auth::user()->name }}
     @else
         <div class="center jumbotron">
             <div class="text-center">
